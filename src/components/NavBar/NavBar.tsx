ffa0ba9391e355bc2dc6a8c53fda8de970dbf13f
Tamara Salinas <gatamara29@gmail.com>
2023-06-17 00:39:56 -0400

    change style css

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./NavBar.css"
 
+
 export const NavBar = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
     return (
         <nav className='navbar'>
-            <ul className="navbar">
+            <div className="hamburguesa" onClick={toggleMenu}>
+                <div className="linea">
+                </div>
+                <div className="linea"></div>
+                <div className="linea"></div>
+            </div>
+            <ul className="navbar-lista">
                 <li><a href="#">Home</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Proyectos</a></li>
